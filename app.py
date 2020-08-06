6240fd6e9f40953e7313a628d3f08d4be7ed71f0
Darumin <jandeo92@gmail.com>
2020-08-06 16:37:59 -0700

    Expanded some definitions

--- app.py
+++ app.py
@@ -1,49 +1,64 @@
-class Item:
-    def __init__(self, true_name: str, description: str=None, placement: str=None):
-        self.name = true_name
-        self.desc = description
-        self.placement = placement
-        self.is_staged = False
-
-    @property
-    def placement_description(self) -> str:
-        return self.placement if self.is_staged else ''
-
-class Container:
-    def __init__(self, true_name):
-        self.name = true_name
-        self.contents = list()
+class Noun:
+    def __init__(self, name: str, description: str):
+        self.name = name
+        self.description = description
 
     def __repr__(self):
-        ret = ''
-        for item in self.items:
-            ret += str(item) + ', '
-        return '\nlabel: ' + self.label + '\nitems: ' + ret + '\n'
+        pass
+
+
+class Item(Noun):
+    def __init__(self, item_name: str, item_desc: str):
+        super().__init__(item_name, item_desc)
+
+        self.can_be_used = False
+        self.is_not_possessed = True
+        self.description_dict = {
+            'item_desc': item_desc,
+            'not_carrying': f'{item_name} is here.',
+            'carrying': f'You are carrying {item_name.lower()}.'
+        }
+
+        self.current_location_desc = self.description_dict['not_carrying']
+
+    def return_desc(self, key: str) -> str:
+        return self.description_dict.get(key)
+
+    def set_new_location_description(self, description: str):
+        self.current_location_desc = description
+
+
+class Container(Noun):
+    def __init__(self, true_name, description):
+        super().__init__(true_name, description)
+
+        self.contents = dict()
+
+    def __repr__(self):
+        pass
 
     def add_item(self, item_name):
-        if not isinstance(item_name, Item):
-            return
-        item_name.is_staged = True
-        self.contents.append(item_name)
+        pass
+
 
 class Room(Container):
-    def __init__(self, true_name: str, description: str=''):
-        super().__init__(true_name)
-        self.desc = description
-        self.compass_rose = dict()
+    def __init__(self, true_name: str, description: str):
+        super().__init__(true_name, description)
 
-    def __str__(self) -> str:
+    def __repr__(self) -> str:
         set_pieces = ''
         for item in self.contents:
-            set_pieces += item.placement_description + ' '
-        return str(self.name + '\n' + self.desc + ' ' + set_pieces)
+            set_pieces += str(item.current_location_desc) + ' '
+        return str(self.name + '\n' + self.description + ' ' + set_pieces)
 
-if __name__ == '__main__':
-    room = Room("The Nice Place", "A big room, quite nice.")
-    rose = Item("A rose", "It smells sweet", "A rose is growing here.")
-    compass = Item("A compass", "Iron, it smells of earth", "The compass is in the corner.")
+    def store_item(self, item: Item, description: str = None):
+        if not description:
+            if item.is_not_possessed:
+                default_desc = item.return_desc('not_carrying')
+            else:
+                default_desc = item.return_desc('carrying')
+        else:
+            default_desc = description
 
-    room.add_item(rose)
-    room.add_item(compass)
-
-    print(room)
+        self.contents[item] = default_desc
+        item.set_new_location_description(default_desc)
