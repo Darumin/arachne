b40e584fb21c099efd9c5cdb9f046e17f2c7e002
Darumin <jandeo92@gmail.com>
2020-08-11 00:50:43 -0700

    Added type hints, exceptions

--- app.py
+++ app.py
@@ -1,68 +1,67 @@
-class Noun:
-    def __init__(self, name):
-        self.name = name
-        self.texts = dict()
+# TODO: Reevaluate whether strict exceptions are needed here.
 
-    def add_description(self, kind, text):
-        key = self.validate_description_kind(kind)
-        self.texts[key] = text
+
+class Noun:
+    def __init__(self, name: str) -> None:
+        self.name: str = name
+        self.texts: dict = dict()
+
+    def add_description(self, kind: str, text: str) -> None:
+        key: str = self.validate_description_kind(kind)
+        self.texts[key]: str = text
         print(kind.capitalize() + f' has been set with key [{key}].')
 
     @staticmethod
-    def validate_description_kind(given_kind):
-        valid_descriptions = {
+    def validate_description_kind(given_kind: str) -> str:
+        valid_descriptions: set = {
             'observation',
             'placement'
         }
 
-        if given_kind in valid_descriptions: return given_kind[:3]
-        raise KeyError(f"'{given_kind}' not valid kind.")
+        if given_kind in valid_descriptions:
+            return given_kind[:3]
+        raise ValueError(f"'{given_kind}' not valid kind.")
 
-    # TODO: Is the repetitive structure below necessary?
-    @staticmethod
-    def validate_container(container_name):
-        return True if isinstance(container_name, Container) else False
 
-    @staticmethod
-    def validate_item(item_name):
-        return True if isinstance(item_name, Item) else False
+class Item(Noun):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+
+    def __repr__(self) -> str:
+        return str(self.texts)
 
 
 class Container(Noun):
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
         super().__init__(name)
 
         self.contents = dict()
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return str(self.contents)
 
-    def add_item(self, item):
-        # TODO: Maybe lump methods together.
-        if not self.validate_item(item):
-            return
+    @staticmethod
+    def _valid_item(item: Item) -> bool:
+        return True if isinstance(item, Item) else False
+
+    def add_item(self, item: Item) -> None:
+        if not self._valid_item(item):
+            raise TypeError(f"Attempted to add '{item}' ({type(item)}). It is not valid [Item]!")
+
         self.contents[item] = f'{item.name.capitalize()} is here.'
 
-    def remove_item(self, item):
-        # TODO: Clean this, somewhat.
-        if not self.validate_item(item):
-            return
+    def remove_item(self, item: Item) -> None:
+        if not self._valid_item(item):
+            raise TypeError(f"Given '{item}' is not desired type. Keys are of type [Item]!")
+
         try:
-            self.contents.get(item.name)
-        finally:
-            self.contents.pop(item.name)
-
-
-class Item(Noun):
-    def __init__(self, name):
-        super().__init__(name)
-
-    def __repr__(self):
-        return str(self.texts)
+            self.contents.pop(item)
+        except KeyError:
+            print("No such key found. Removal failed.")
 
 
 class Room(Container):
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
         super().__init__(name)
 
         compass_rose = dict()
@@ -70,6 +69,7 @@ class Room(Container):
 
     def __repr__(self) -> str:
         # TODO: Probably need to move this to a separate function.
+        # This should instead return a debugger representation.
         room_desc = self.texts['obs']
         props = ''
         for item in self.contents:
--- navigation.py
+++ navigation.py
@@ -2,6 +2,6 @@ from app import Noun
 
 
 class Player(Noun):
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
         super().__init__(name)
         # TODO: Implement basic location tracking, verbs, noun behaviors
