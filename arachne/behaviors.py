6be67c0e1c2b4b0ceda0738958b106128f343fcb
Darumin <jandeo92@gmail.com>
2020-08-28 20:54:04 -0700

    mass flattening

--- arachne/behaviors.py
+++ arachne/behaviors.py
@@ -1,256 +1,221 @@
-from typing import Any
 from functools import partial
 
 from arachne.lingo import Object, Verb
 from arachne.game import _Player, _Game
-from arachne.nouns import Container, Item
+from arachne.nouns import Noun, Container, Item, Room
+
+
 # the bulk of game behavior is found here
+def return_attribute(given: Noun, attribute: str) -> str:
+    return getattr(given, attribute)
 
 
-class Behavior:
-    @staticmethod
-    def return_attribute(given, attribute) -> str:
-        """
-        :param given: Noun
-        :param attribute: str
-        """
-        return getattr(given, attribute)
+return_name = partial(return_attribute, attribute="name")
+return_examined = partial(return_attribute, attribute="when_examined")
+return_contents = partial(return_attribute, attribute="contents")
+return_encountered = partial(return_attribute, attribute="when_encountered")
+return_unlock_id = partial(return_attribute, attribute="unlock_id")
 
-    @staticmethod
-    def set_start(room) -> None:
-        """
-        :param room: Room
-        """
-        _Game._start_location = room
-        Behavior.set_player_location(room)
 
-    @staticmethod
-    def room_description(room) -> str:
-        """
-        :param room: Room
-        """
-        desc: str = return_name(room)
-        desc += "\n" + return_examined(room)
-        desc += Behavior.room_placed_description(room)
-        return desc
+def set_start(room: Room) -> None:
+    _Game._start_location = room
+    set_player_location(room)
 
-    @staticmethod
-    def room_placed_description(room) -> str:
-        """
-        :param room: Room
-        """
-        extra: dict = return_contents(room)
-        desc: str = ""
-        if extra:
-            desc += "\n\n"
-            for each in extra.values():
-                if each.is_concealed is False:
-                    desc += return_encountered(each) + " "
-        return desc
 
-    @staticmethod
-    def _inventory_values():
-        return _Player.contents.values()
+def room_description(room: Room) -> str:
+    desc: str = return_name(room)
+    desc += "\n" + return_examined(room)
+    desc += _room_placed_description(room)
+    return desc
 
-    @staticmethod
-    def _inventory_keys():
-        return _Player.contents.keys()
 
-    @staticmethod
-    def _inventory_dict():
-        return _Player.contents
+def _room_placed_description(room: Room) -> str:
+    extra: dict = return_contents(room)
+    desc: str = ""
+    if extra:
+        desc += "\n\n"
+        for each in extra.values():
+            if each.is_concealed is False:
+                desc += return_encountered(each) + " "
+    return desc
 
-    @staticmethod
-    def describe_inventory():
-        carrying = list(Behavior._inventory_values())
 
-        if any(Behavior._inventory_dict()):
-            description = "You are carrying... \n"
-            if len(carrying) > 1:
-                for item in carrying[:-1]:
-                    description += item.name + ", "
-                description += "and " + carrying[-1].name + "."
-            else:
-                description += carrying[0].name + "."
-        else: description = "You are not carrying anything."
-        return description
+def _inventory() -> dict:
+    return _Player.contents
 
-    @staticmethod
-    def check_inventory(item_str: str) -> bool:
-        inv = dict(Behavior._inventory_dict())
-        for item_id in inv:
-            instance = inv[item_id]
-            if item_str in instance.name:
-                return True
-        return False
 
-    @staticmethod
-    def add_to_inventory(item) -> None:
-        """
-        :param item: Item
-        """
-        Behavior.add_to_container(_Player, item)
+def describe_inventory() -> str:
+    carrying: list = list(_inventory().values())
 
-    @staticmethod
-    def free_item(item):
-        """
-        :param item: Item
-        """
-        parent = item.parent_container
-        if parent:
-            item.parent_container = None
-            Behavior.remove_from_container(parent, item)
-
-    @staticmethod
-    def player_location():
-        return _Player.current_location
-
-    @staticmethod
-    def set_player_location(room: Any):
-        _Player.current_location = room
-
-    @staticmethod
-    def vicinity() -> dict:
-        open_containers = Behavior.get_all_open_containers()
-        inventory = Behavior._inventory_dict()
-        return {**open_containers, **inventory}
-
-    @staticmethod
-    def get_all_open_containers() -> dict:
-        current_location = Behavior.player_location()
-        all_containers = dict()
-        for each in current_location.contents:
-            noun = current_location.contents[each]
-            if isinstance(noun, Container) and not noun.is_sealed:
-                all_containers.update(noun.contents)
-        return {**current_location.contents, **all_containers}
-
-    @staticmethod
-    def add_to_container(container, item):
-        """
-        :param container: Room or Container
-        :param item: Item
-        """
-        _key: int = id(item)
-        if _key not in container.contents:
-            container.contents[_key] = item
-            item.parent_container = container
-
-    @staticmethod
-    def drop_in_room(item):
-        """
-        :param item: Item
-        """
-        item.when_encountered = f"{item.name.capitalize()} is here."
-        room_dropped = Behavior.player_location()
-        Behavior.add_to_container(room_dropped, item)
-
-    @staticmethod
-    def remove_from_container(container: Container, item: Item):
-        """
-        :param container: Container
-        :param item: Item
-        """
-        container.contents.pop(id(item))
-
-    @staticmethod
-    def describe_contents(container: Container):
-        if container.is_sealed:
-            return "\n\nIt is closed."
-
-        elif container.contents:
-            description = "\n\n"
-            values = iter(container.contents.values())
-
-            if len(container.contents) == 1:
-                description += f"Inside is {next(values).name}."
-            else:
-                for item in values:
-                    description += item.name + ", "
-                description = "\n\nInside it are: " + description[2:-2] + "."
-            return description
-        return ""
-
-    @staticmethod
-    def guess_object(object_str: str) -> tuple:
-        """
-        :return: tuple of (Object, list)
-        """
-        results: list = list()
-        if object_str == "": return Object.UNSPECIFIED, results
-
-        # check vicinity (everywhere except inventory) for closest matching subject
-        vicinity: dict = Behavior.vicinity()
-        for object_id in vicinity:
-            instance = vicinity[object_id]
-            if object_str in instance.name \
-                    and not instance.is_concealed:
-                results.append(instance)
-
-        # check what kind of subject input this is
-        typified = Behavior.typify_object(object_str, len(results))
-
-        # if multiple matches, resolve the matches
-        # if nonexistent, create a list with one entry for the sake of returning a valid tuple
-        if typified is Object.MULTIPLE: return Behavior.resolve_multiple(results)
-        if typified is Object.NONEXISTENT: results.append(Object.NONEXISTENT)
-
-        # finally return this tuple if nothing else applies
-        return typified, results[0]
-
-    @staticmethod
-    def typify_object(object_str: str, amount_found: int) -> Object:
-        if object_str == "all": return Object.ALL
-        if amount_found == 0: return Object.NONEXISTENT
-        if amount_found > 1: return Object.MULTIPLE
-        if Behavior.check_inventory(object_str): return Object.POSSESSED
-        return Object.FOUND
-
-    @staticmethod
-    def resolve_multiple(results: list):
-        print("Which one?")
-
-        for result in enumerate(results, start=1):
-            print(f"{result[0]})", result[1].name, end=" ")
-
-        choice: str = input("\n>")
-        return Behavior.guess_object(choice)
-
-    @staticmethod
-    def check_for_key(container: Container) -> bool:
-        for each in Behavior._inventory_values():
-            unlock_id = return_unlock_id(each)
-            if unlock_id == id(container):
-                return True
-        return False
-
-    @staticmethod
-    def lock_outputs(verb: Verb, container: Container):
-        if isinstance(container, Container):
-            key_found = Behavior.check_for_key(container)
-            if key_found:
-                if verb is Verb.UNLOCK:
-                    if not container.is_sealed:
-                        print("This is already unlocked.")
-                        return
-                    else:
-                        container.is_sealed = False
-                        print(f"{container.name.capitalize()} has been unlocked.")
-                elif verb is Verb.LOCK:
-                    if container.is_sealed:
-                        print("This is already locked.")
-                        return
-                    else:
-                        container.is_sealed = True
-                        print(f"{container.name.capitalize()} has been locked.")
-            else:
-                print("You can't do that without a key.")
+    if any(_inventory()):
+        description: str = "You are carrying... \n"
+        if len(carrying) > 1:
+            for item in carrying[:-1]:
+                description += item.name + ", "
+            description += "and " + carrying[-1].name + "."
         else:
-            print("That is not something you can do that to.")
+            description += carrying[0].name + "."
+    else: description = "You are not carrying anything."
+    return description
 
 
-return_name = partial(Behavior.return_attribute, attribute="name")
-return_examined = partial(Behavior.return_attribute, attribute="when_examined")
-return_contents = partial(Behavior.return_attribute, attribute="contents")
-return_encountered = partial(Behavior.return_attribute, attribute="when_encountered")
-return_unlock_id = partial(Behavior.return_attribute, attribute="unlock_id")
+def check_inventory(item_str: str) -> bool:
+    inv: dict = _inventory()
+    for item_id in inv:
+        instance = inv[item_id]
+        if item_str in instance.name:
+            return True
+    return False
+
+
+def add_to_inventory(item: Item) -> None:
+    add_to_container(_Player, item)
+
+
+def free_item(item: Item):
+    parent = item.parent_container
+    if parent:
+        item.parent_container = None
+        remove_from_container(parent, item)
+
+
+def get_player_location():
+    return _Player.current_location
+
+
+def set_player_location(room: Room):
+    _Player.current_location = room
+
+
+def vicinity() -> dict:
+    # lump everything within reach of player into one dict
+    open_containers = get_all_open_containers()
+    inventory = _inventory()
+    return {**open_containers, **inventory}
+
+
+def get_all_open_containers() -> dict:
+    current_location = get_player_location()
+    all_containers = dict()
+
+    for each in current_location.contents:
+        noun = current_location.contents[each]
+        if isinstance(noun, Container) and not noun.is_sealed:
+            all_containers.update(noun.contents)
+
+    return {**current_location.contents, **all_containers}
+
+
+def add_to_container(subject, item):
+    """
+    :param subject: can be type _Player, Container, Room
+    :param item: can be any type where gettable = True
+    any subject may have contents containing items. The _Player's contents is inventory.
+    see usage of subject in remove_from_container() as well
+    """
+    _key: int = id(item)
+    if _key not in subject.contents:
+        subject.contents[_key] = item
+        item.parent_container = subject
+
+
+def drop_in_room(item: Item):
+    item.when_encountered = f"{item.name.capitalize()} is here."
+    room_dropped = get_player_location()
+    add_to_container(room_dropped, item)
+
+
+def remove_from_container(subject, item: Item):
+    subject.contents.pop(id(item))
+
+
+def describe_contents(container: Container):
+    if container.is_sealed:
+        return "\n\nIt is closed."
+
+    elif container.contents:
+        description = "\n\n"
+        values = iter(container.contents.values())
+
+        if len(container.contents) == 1:
+            description += f"Inside is {next(values).name}."
+        else:
+            for item in values:
+                description += item.name + ", "
+            description = "\n\nInside it are: " + description[2:-2] + "."
+        return description
+    return ""
+
+
+def guess_object(object_str: str) -> tuple:
+    results: list = list()
+    if object_str == "": return Object.UNSPECIFIED, results
+
+    # check vicinity (everywhere except inventory) for closest matching subject
+    vic: dict = vicinity()
+    for object_id in vic:
+        instance = vic[object_id]
+        if object_str in instance.name \
+                and not instance.is_concealed:
+            results.append(instance)
+
+    # check what kind of subject input this is
+    typified = typify_object(object_str, len(results))
+
+    # if multiple matches, resolve the matches
+    # if nonexistent, create a list with one entry for the sake of returning a valid tuple
+    if typified is Object.MULTIPLE: return _resolve_multiple(results)
+    if typified is Object.NONEXISTENT: results.append(Object.NONEXISTENT)
+
+    # finally return this tuple if nothing else applies
+    return typified, results[0]
+
+
+def typify_object(object_str: str, amount_found: int) -> Object:
+    if object_str == "all": return Object.ALL
+    if amount_found == 0: return Object.NONEXISTENT
+    if amount_found > 1: return Object.MULTIPLE
+    if check_inventory(object_str): return Object.POSSESSED
+    return Object.FOUND
+
+
+def _resolve_multiple(results: list):
+    print("Which one?")
+
+    for result in enumerate(results, start=1):
+        print(f"{result[0]})", result[1].name, end=" ")
+
+    choice: str = input("\n>")
+    return guess_object(choice)
+
+
+def check_for_key(container: Container) -> bool:
+    for each in _inventory().values():
+        unlock_id = return_unlock_id(each)
+        if unlock_id == id(container):
+            return True
+    return False
+
+
+def lock_outputs(verb: Verb, container: Container):
+    if isinstance(container, Container):
+        key_found = check_for_key(container)
+        if key_found:
+            if verb is Verb.UNLOCK:
+                if not container.is_sealed:
+                    print("This is already unlocked.")
+                    return
+                else:
+                    container.is_sealed = False
+                    print(f"{container.name.capitalize()} has been unlocked.")
+            elif verb is Verb.LOCK:
+                if container.is_sealed:
+                    print("This is already locked.")
+                    return
+                else:
+                    container.is_sealed = True
+                    print(f"{container.name.capitalize()} has been locked.")
+        else:
+            print("You can't do that without a key.")
+    else:
+        print("That is not something you can do that to.")
--- arachne/parser.py
+++ arachne/parser.py
@@ -1,7 +1,8 @@
 # TODO: Need to implement OPEN and CLOSE
+import arachne.behaviors as be
+
 from arachne.lexer import tokenize
 from arachne.lingo import Verb, Object, Prep, Compass
-from arachne.behaviors import Behavior as be
 # this is where input is parsed from passed tokens, then resolves into turn event
 
 
@@ -16,128 +17,126 @@ def write_action(input_str: str) -> None:
         print("GO")
         return
 
-    if verb is Verb.LOOK: return Parser.look()
-    if verb is Verb.TAKE: return Parser.take(results)
-    if verb is Verb.DROP: return Parser.drop(results)
-    if verb is Verb.EXAMINE: return Parser.examine(results)
-    if verb is Verb.PUT: return Parser.put(results)
-    if verb is Verb.INVENTORY: return Parser.inventory()
+    if verb is Verb.LOOK: return look()
+    if verb is Verb.TAKE: return take(results)
+    if verb is Verb.DROP: return drop(results)
+    if verb is Verb.EXAMINE: return examine(results)
+    if verb is Verb.PUT: return put(results)
+    if verb is Verb.INVENTORY: return inventory()
 
-    if verb is Verb.UNLOCK or verb is Verb.LOCK: return Parser.lock_switch(verb, results)
+    if verb is Verb.UNLOCK or verb is Verb.LOCK: return lock_switch(verb, results)
     if verb is Verb.OPEN or verb is Verb.CLOSE: pass
 
-    return Parser.lecture_player()
+    return lecture_player()
 
 
-class Parser:
-    @staticmethod
-    def look() -> None:
-        print(be.room_description(be.player_location()))
+def look() -> None:
+    print(be.room_description(be.get_player_location()))
 
-    @staticmethod
-    def take(results: list) -> None:
-        # we only want to identify one object and ignore everything else. from that identification
-        # derive a target of type Object, and an obj of type Item.
-        given_obj = Parser._get_obj_str(results)
-        target, obj = be.guess_object(given_obj)
 
-        if target is Object.POSSESSED: print(f"You're carrying that -> '{obj.name}'")
-        elif target is Object.UNSPECIFIED: print("Please specify what you want to take, i.e. \"take the crown\"")
-        elif target is Object.NONEXISTENT: print(f"This isn't available -> '{given_obj}'")
-        elif target is Object.FOUND:
-            if obj.is_gettable:
-                be.free_item(obj)
-                be.add_to_inventory(obj)
-                print(f"You take {obj.name.lower()}.")
-            else: print(f"That can't be taken. -> {obj.name}")
+def take(results: list) -> None:
+    # we only want to identify one object and ignore everything else. from that identification
+    # derive a target of type Object, and an obj of type Item.
+    given_obj = _get_obj_str(results)
+    target, obj = be.guess_object(given_obj)
 
-    @staticmethod
-    def drop(results: list):
-        given_obj = Parser._get_obj_str(results)
-        target, obj = be.guess_object(given_obj)
-
-        if target is Object.UNSPECIFIED: print("Please specify what you want to drop, i.e. \"drop the mic\"")
-        elif target is Object.NONEXISTENT: print(f"This isn't available -> '{given_obj}'")
-        elif target is Object.FOUND: print(f"You are not carrying this -> '{given_obj}'")
-        elif target is Object.POSSESSED:
+    if target is Object.POSSESSED: print(f"You're carrying that -> '{obj.name}'")
+    elif target is Object.UNSPECIFIED: print("Please specify what you want to take, i.e. \"take the crown\"")
+    elif target is Object.NONEXISTENT: print(f"This isn't available -> '{given_obj}'")
+    elif target is Object.FOUND:
+        if obj.is_gettable:
             be.free_item(obj)
-            be.drop_in_room(obj)
-            print(f"You drop {obj.name.lower()}.")
+            be.add_to_inventory(obj)
+            print(f"You take {obj.name.lower()}.")
+        else: print(f"That can't be taken. -> {obj.name}")
 
-    @staticmethod
-    def examine(results: list) -> None:
-        given_obj = Parser._get_obj_str(results)
-        target, obj = be.guess_object(given_obj)
 
-        if target is Object.UNSPECIFIED: print("Please specify what you want to examine, i.e. \"examine dog\"")
-        elif target is Object.NONEXISTENT: print(f"This isn't available -> '{given_obj}'")
-        elif target is Object.FOUND or target is Object.POSSESSED:
-            description = obj.when_examined
-            if obj.has_contents:
-                description += be.describe_contents(obj)
-            print(description)
+def drop(results: list):
+    given_obj = _get_obj_str(results)
+    target, obj = be.guess_object(given_obj)
 
-    @staticmethod
-    def lock_switch(verb: Verb, results: list):
-        given_obj = Parser._get_obj_str(results)
-        target, obj = be.guess_object(given_obj)
+    if target is Object.UNSPECIFIED: print("Please specify what you want to drop, i.e. \"drop the mic\"")
+    elif target is Object.NONEXISTENT: print(f"This isn't available -> '{given_obj}'")
+    elif target is Object.FOUND: print(f"You are not carrying this -> '{given_obj}'")
+    elif target is Object.POSSESSED:
+        be.free_item(obj)
+        be.drop_in_room(obj)
+        print(f"You drop {obj.name.lower()}.")
 
-        if target is Object.UNSPECIFIED: print("What do you want to do that to?")
-        elif target is Object.NONEXISTENT: print(f"This isn't available -> '{given_obj}'")
-        elif target is Object.FOUND or target is Object.POSSESSED:
-            be.lock_outputs(verb, obj)
 
-    @staticmethod
-    def put(results: list):
-        obj_list, preposition = Parser._extend_with_prep(results)
-        if not obj_list or not preposition:
-            print("Please specify what you want to do, i.e. \"put coin in box\"")
-            return
+def examine(results: list) -> None:
+    given_obj = _get_obj_str(results)
+    target, obj = be.guess_object(given_obj)
 
-        if len(obj_list) == 1:
-            print(f"{obj_list[0][1].capitalize()} {preposition[0][1]} what?")
-            return
+    if target is Object.UNSPECIFIED: print("Please specify what you want to examine, i.e. \"examine dog\"")
+    elif target is Object.NONEXISTENT: print(f"This isn't available -> '{given_obj}'")
+    elif target is Object.FOUND or target is Object.POSSESSED:
+        description = obj.when_examined
+        if obj.has_contents:
+            description += be.describe_contents(obj)
+        print(description)
 
-        target_one, obj_one = be.guess_object(obj_list[0][1])
-        target_two, obj_two = be.guess_object(obj_list[1][1])
 
-        if obj_one is Object.NONEXISTENT or obj_two is Object.NONEXISTENT:
-            print("There is no such thing.")
-            return
+def lock_switch(verb: Verb, results: list):
+    given_obj = _get_obj_str(results)
+    target, obj = be.guess_object(given_obj)
 
-        if preposition[0][0] is Prep.WITHIN:
-            if obj_one.is_gettable:
-                be.free_item(obj_one)
-                be.add_to_container(obj_two, obj_one)
-                print(f"You put {obj_one.name} in {obj_two.name}.")
-            else:
-                print("That can't be stored.")
+    if target is Object.UNSPECIFIED: print("What do you want to do that to?")
+    elif target is Object.NONEXISTENT: print(f"This isn't available -> '{given_obj}'")
+    elif target is Object.FOUND or target is Object.POSSESSED:
+        be.lock_outputs(verb, obj)
 
-    @staticmethod
-    def inventory():
-        print(be.describe_inventory())
 
-    @staticmethod
-    def lecture_player() -> None:
-        print("That is not a good input.")
+def put(results: list):
+    obj_list, preposition = _extend_with_prep(results)
+    if not obj_list or not preposition:
+        print("Please specify what you want to do, i.e. \"put coin in box\"")
+        return
 
-    @staticmethod
-    def _get_obj_str(results: list) -> str:
-        given_obj = ""
+    if len(obj_list) == 1:
+        print(f"{obj_list[0][1].capitalize()} {preposition[0][1]} what?")
+        return
 
-        for each in results:
-            if isinstance(each[0], Object):
-                given_obj = each[1]
-        return given_obj
+    target_one, obj_one = be.guess_object(obj_list[0][1])
+    target_two, obj_two = be.guess_object(obj_list[1][1])
 
-    @staticmethod
-    def _extend_with_prep(results: list) -> tuple:
-        extends, prep = list(), list()
+    if obj_one is Object.NONEXISTENT or obj_two is Object.NONEXISTENT:
+        print("There is no such thing.")
+        return
 
-        for each in results:
-            if isinstance(each[0], Object):
-                extends.append(each)
-            elif isinstance(each[0], Prep):
-                prep.append(each)
+    if preposition[0][0] is Prep.WITHIN:
+        if obj_one.is_gettable:
+            be.free_item(obj_one)
+            be.add_to_container(obj_two, obj_one)
+            print(f"You put {obj_one.name} in {obj_two.name}.")
+        else:
+            print("That can't be stored.")
 
-        return extends, prep
+
+def inventory():
+    print(be.describe_inventory())
+
+
+def lecture_player() -> None:
+    print("That is not a good input.")
+
+
+def _get_obj_str(results: list) -> str:
+    given_obj = ""
+
+    for each in results:
+        if isinstance(each[0], Object):
+            given_obj = each[1]
+    return given_obj
+
+
+def _extend_with_prep(results: list) -> tuple:
+    extends, prep = list(), list()
+
+    for each in results:
+        if isinstance(each[0], Object):
+            extends.append(each)
+        elif isinstance(each[0], Prep):
+            prep.append(each)
+
+    return extends, prep
